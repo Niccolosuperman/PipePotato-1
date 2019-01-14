60eed29290692d8d2a4b34ac38661169048ef699
Himmet Karahan Buhan <me@karahanbuhan.com>
2019-01-14 20:13:02 +0300

    Get plugin via ModuleManage's getClass in auth module

--- src/main/kotlin/module/auth/AuthModule.kt
+++ src/main/kotlin/module/auth/AuthModule.kt
@@ -1,12 +1,16 @@
 package io.github.pipespotatos.module.auth
 
+import io.github.pipespotatos.PipePotatoPlugin
 import io.github.pipespotatos.api.module.Module
+import io.github.pipespotatos.api.module.ModuleManager
 import io.github.pipespotatos.module.auth.command.CommandSpecs
 import io.github.pipespotatos.module.auth.listener.PlayerActionListener
 import io.github.pipespotatos.module.auth.listener.PlayerJoinQuitListener
 import org.spongepowered.api.Sponge
 
 class AuthModule : Module("auth", "Auth") {
+
+    private val plugin = ModuleManager.getClass<PipePotatoPlugin>()
 
     override fun onEnable() {
         registerCommands()
@@ -22,15 +26,15 @@ class AuthModule : Module("auth", "Auth") {
     private fun registerCommands() {
         val commandManager = Sponge.getCommandManager()
 
-        commandManager.register(this, CommandSpecs.getLoginCommandSpec(), "login", "l")
-        commandManager.register(this, CommandSpecs.getRegisterCommandSpec(), "register", "reg")
+        commandManager.register(plugin, CommandSpecs.getLoginCommandSpec(), "login", "l")
+        commandManager.register(plugin, CommandSpecs.getRegisterCommandSpec(), "register", "reg")
     }
 
     private fun registerEvents() {
         val eventManager = Sponge.getEventManager()
 
-        eventManager.registerListeners(this, PlayerActionListener())
-        eventManager.registerListeners(this, PlayerJoinQuitListener())
+        eventManager.registerListeners(plugin, PlayerActionListener())
+        eventManager.registerListeners(plugin, PlayerJoinQuitListener())
     }
 
 }
