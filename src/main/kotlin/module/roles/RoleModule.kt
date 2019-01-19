450b9391f75c7bf41f72ab8e21ee0612cb4b4ac6
Ecmel B. CANLIER <me@ecmelberk.com>
2019-01-19 17:24:15 +0300

    Configuration API

--- src/main/kotlin/module/roles/RoleModule.kt
+++ src/main/kotlin/module/roles/RoleModule.kt
@@ -1,26 +1,21 @@
 package module.roles
 
-import com.google.common.reflect.TypeToken
 import io.github.pipespotatos.PipePotatoPlugin
+import io.github.pipespotatos.api.config.loadConfig
 import io.github.pipespotatos.api.module.Module
 import io.github.pipespotatos.api.module.ModuleManager
-import io.github.pipespotatos.extensions.ifNull
 import io.github.pipespotatos.module.roles.RoleManager
 import io.github.pipespotatos.module.roles.command.AddRoleCommand
 import io.github.pipespotatos.module.roles.command.DelRoleCommand
-import ninja.leaping.configurate.hocon.HoconConfigurationLoader
-import ninja.leaping.configurate.objectmapping.ObjectMappingException
 import org.spongepowered.api.Sponge
 import org.spongepowered.api.command.args.GenericArguments
 import org.spongepowered.api.command.spec.CommandSpec
 import org.spongepowered.api.text.Text
 import java.io.File
-import java.io.IOException
 
 class RoleModule : Module("role", "Role") {
 
     private val plugin = ModuleManager.getClass<PipePotatoPlugin>()
-    private lateinit var config: Config
 
     override fun onEnable() {
         config()
@@ -28,29 +23,10 @@ class RoleModule : Module("role", "Role") {
     }
 
     private fun config() {
-        val configManager = HoconConfigurationLoader.builder().setPath(File("./roles.conf").toPath()).build()
+        val config = loadConfig<Config>(File("./config/roles.conf"))
 
-        try {
-            this.config = configManager.load().getValue(TypeToken.of(Config::class.java)).ifNull {
-                this.logger.info("Config is null, generating...")
-
-                val node = configManager.createEmptyNode()
-                node.setValue(TypeToken.of(Config::class.java), Config())
-
-                configManager.save(node)
-
-                Config()
-            }
-        } catch (e: ObjectMappingException) {
-            this.logger.error("Failed to load the config - Using a default", e)
-            this.config = Config()
-        } catch (e: IOException) {
-            this.logger.error("Failed to load the config - Using a default", e)
-            this.config = Config()
-        }
-
-        RoleManager.setConfig(this.config)
-        this.logger.info("Loaded ${this.config.roles.size} roles!")
+        RoleManager.setConfig(config)
+        this.logger.info("Loaded ${config.roles.size} roles!")
     }
 
     private fun commands() {
