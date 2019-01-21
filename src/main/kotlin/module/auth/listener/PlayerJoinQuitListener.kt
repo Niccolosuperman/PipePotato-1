b47777aa951a74c441d040d28cf6a832c7ff9881
Ecmel B. CANLIER <me@ecmelberk.com>
2019-01-21 18:28:49 +0300

    Login timer

--- src/main/kotlin/module/auth/listener/PlayerJoinQuitListener.kt
+++ src/main/kotlin/module/auth/listener/PlayerJoinQuitListener.kt
@@ -1,12 +1,18 @@
 package io.github.pipespotatos.module.auth.listener
 
+import io.github.pipespotatos.PipePotatoPlugin
+import io.github.pipespotatos.api.module.ModuleManager
 import io.github.pipespotatos.module.auth.NotLoggedException
 import io.github.pipespotatos.module.auth.player.AuthPlayerManager
 import org.spongepowered.api.event.Listener
 import org.spongepowered.api.event.network.ClientConnectionEvent
+import org.spongepowered.api.scheduler.Task
 import org.spongepowered.api.text.Text
+import java.util.concurrent.TimeUnit
 
 class PlayerJoinQuitListener {
+
+    private val plugin = ModuleManager.getClass<PipePotatoPlugin>()
 
     @Listener
     fun onPlayerJoin(event: ClientConnectionEvent.Join) {
@@ -17,6 +23,12 @@ class PlayerJoinQuitListener {
             player.sendMessage(Text.of("You are not registered, register via /register <password> <verify>"))
         else
             player.sendMessage(Text.of("You must log in. Usage is /login <password>"))
+
+        Task.builder().execute(Runnable {
+            if (!authWallPlayer.isLogged) {
+                player.kick()
+            }
+        }).delay(10, TimeUnit.SECONDS).submit(plugin) // @todo Config
     }
 
     @Listener
