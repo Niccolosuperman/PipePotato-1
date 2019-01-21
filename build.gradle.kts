857e49e426f62d1335f90a4720d3a126b7614247
Himmet Karahan Buhan <root@karahanbuhan.com>
2019-01-21 22:33:57 +0300

    Version bump to 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.pipespotatos"
-version = "0.1.0"
+version = "0.2.0"
 description = "Core plugin which features all the essentials and mechanics of the official server"
 
 repositories {
